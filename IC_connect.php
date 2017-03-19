aeddc73faef2b2baf9723ae56ff5b43b8c6bf44b
dnmarks <asia.n.marks@gmail.com>
2017-03-19 03:05:41 -0700

    Update IC_connect.php

--- IC_connect.php
+++ IC_connect.php
@@ -1,14 +1,15 @@
 <?php
-$db = mysqli_connect('107.180.57.111', 'deasia', 'Deadfly2k');
+
+$db = mysqli_connect('localhost', 'root', '');
 if ($db){
 	//print "Connected! ";
-	//$create_db = mysqli_query($db, 'CREATE DATABASE dm6_cards');
-	$select_db = mysqli_select_db($db, 'dm6_cards');
+	$create_db = mysqli_query($db, 'CREATE DATABASE cards');
+	$select_db = mysqli_select_db($db, 'cards');
 } else {
-	//print "Could not connect! ";
+	print "Could not connect! ";
 }
 
-if (/*$create_db &&*/ $select_db) {
+if ($create_db && $select_db) {
 	$table_query = 'CREATE TABLE index_cards (
 	id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY, 
 	word VARCHAR(100), 
@@ -18,10 +19,10 @@ if (/*$create_db &&*/ $select_db) {
 	$problem = false; 
 	
 	if(!$problem){
-		//print "Your table has been created";
+		print "Your table has been created";
 	};
 } else {
-	print "Could not create table...";
+//	print "Could not create table...";
 };
 
 
